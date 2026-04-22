f16b352bb7a144301512449b8d0c3aed0eb2bead
Tshephi-dust <abc25-007@thuto.bac.ac.bw>
2026-04-22 16:24:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function updateLoveCounter() {
 
 updateLoveCounter();
 setInterval(updateLoveCounter, 86400000); // Update every day
+// Lightbox Effect for Gallery Images
+const galleryImages = document.querySelectorAll('.gallery-img');
+
+galleryImages.forEach(img => {
+    img.addEventListener('click', function() {
+        const imgSrc = img.getAttribute('src');
+        const lightbox = document.createElement('div');
+        lightbox.classList.add('lightbox');
+        lightbox.innerHTML = `<img src="${imgSrc}" class="lightbox-img"><span class="close">&times;</span>`;
+        document.body.appendChild(lightbox);
+
+        // Close the lightbox when clicked
+        const closeButton = lightbox.querySelector('.close');
+        closeButton.addEventListener('click', () => {
+            document.body.removeChild(lightbox);
+        });
+    });
+});
